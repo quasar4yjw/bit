fdf50585a6eccff0945d0dfeb3356e5435d24c01
HK.CHO <j880825@gmail.com>
2014-10-14 20:43:40 +0900

    ㅇㅇ

--- node01/hello-world-server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer(function handler(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-	console.log('okok33');
-}).listen(1337, '192.168.0.37');
-console.log('Server running at http://127.0.0.1:1337/');
